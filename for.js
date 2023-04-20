c8d796a5b1127b70cf73c910d6b5c6a2ba42eb6e
Natalie Rodriguez <codinggeographer@gmail.com>
2023-04-20 07:33:44 -0600

    add for loop to iterate through array and print words with length of 3

--- for.js
+++ for.js
@@ -23,6 +23,16 @@ for(var i = 0; i < weather_options.length; i++){
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+// for loop is setting a counter as 0, as long as the counter is less than
+// the length of the words array, it will execute the if statement in the code block
+for(var i = 0; i < words.length; i++){
+  if(words[i].length == 3){
+    console.log(words[i])
+  }
+  // The if statement is checking the length of the element(word), if the length is 
+  // equal to 3, it will be output in to the console. 
+}
+
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
   // Use technical vocabulary and explain each part to demonstrate your understanding! 
